8e2eb3952361a570e00ffb0779748ff8cbca38b5
mikale <locifer@gmail.com>
2013-11-27 22:42:36 +0800

    add some extra files

---
+++ app/controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\controllers;
+
+class UserController extends \lithium\action\Controller {
+
+    public function register(){
+
+    }
+
+    public function login(){
+
+    }
+
+}
